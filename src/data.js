160b3ebca52b16922e84c161bae1bcb3427b667d
Whitney Harden <wtharden320@gmail.com>
2022-07-25 22:37:31 -0400

    data fix

--- src/data.js
+++ src/data.js
@@ -14,7 +14,7 @@ const data = {
       verse1: ['My eyes ain\'t use to these rays', 'I\'m feeling exposed, but I can\'t hide no more', 'I can\'t hide', 'As the sun shines on all of my glory', 'My flaws don\'t look so bad at all', 'What was I so afraid of?'],
       hook: ['Every part of me is a vision of a portrait, of Mona, Mona Lisa', 'Every part of me is beautiful', 'And I finally see I\'m a work of art', 'A masterpiece'],
       verse2: ['Who is this I tried so long to fight', 'Filling my head with lies that I\'m not good enough', 'Then I heard something in my ear','Tell me I\'m perfect, now that I know the truth', 'Time to show and prove' ],
-      bridge: ['And now I see the pretty colors on my canvas', 'I\'m a work art, a Mona Lisa', 'I\ll share my picture with the world', 'I\'m not afraid to let it show anymore'],
+      bridge: ['And now I see the pretty colors on my canvas', 'I\'m a work art, a Mona Lisa', 'I\'ll share my picture with the world', 'I\'m not afraid to let it show anymore'],
       chorus: ['I can light the night, shine so bright (shine so bright)(Mona Lisa)', '(Let my colors paint the sky)', 'I can light the night, shine so bright', '(there is beauty in my eyes)', '(Took a while to see that I\'m a work of art that I\'m a masterpiece)', 'I am beautiful'],
       closing: ['Every part of me is a vision', 'Every part of me is beautiful', 'and I finally see, I\'m a work of art', 'a masterpiece']
     }
