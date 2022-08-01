28b367084446d71d1fd5dcf66637f86e84f83d5b
Whitney Harden <wtharden320@gmail.com>
2022-08-01 00:29:58 -0400

    fixed flow of data from feed to entry

--- src/component/Entry.js
+++ src/component/Entry.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 function Entry(props){
-  const keywords = props.data.entry1.keywords;
+  const [keywords, setKeywords] = useState(props.entry.keywords);
+  
   const displayKewords = keywords.map( (word,index) => {
     return(
       <li key={index}>{word}</li>
     )
   });
-
+  const {title, author} = props.entry;
   return(
     <React.Fragment>
-      <h3>This is the Entry song title: {props.data.entry1.title}</h3>
-      <p>This is the author of the song: {props.data.entry1.author}</p>
+      <h3>This is the Entry song title: {title}</h3>
+      <p>This is the author of the song: {author}</p>
       <h3>Song keywords: </h3>
       <ul>{displayKewords}</ul>
-      <h4>{props.data.entry1.title}</h4>
-      <h4>{`returnEntryId: ${props.data.entry1.returnEntryId()}`}</h4>
+      <h4>{title}</h4>
     </React.Fragment>
   )
 }
--- src/component/Feed.js
+++ src/component/Feed.js
@@ -1,41 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Entry from './Entry';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Feed(props){
-  const {data} = props;
-  console.log(data);
-  const getEntry = i => {
-    //get data
-    console.log(i);
-    console.log(data);
-    //for each key or entry assign to a variable and return it
-    console.log('constructor');
-    console.log(data.constructor.entries.length);
-    //should I create an array
+  const [data, setData] = useState(props.data);
+  const [entryArr, setEntryArr] = useState([]);
+
+  useEffect(() => {
+    console.log(data == entryArr);
+    console.log(entryArr);
+    setEntryArr(getEntry(data));
+  }, [data]);
+
+  const getEntry = obj => {
+    const keys = [];
+    for (let key in obj){
+      keys.push(key);
+    }
+    return keys;
   }
+
+  const displayEntries = entryArr.map( (entry, index) => {
+    console.log(data);
+    console.log(data[entry]);
+    return(
+      <Col>
+        <Entry entry={data[entry]} />
+      </Col>
+    )
+  })
+
   return(
     <Container>
       <Row>
+        {displayEntries}
         <Col>
-          <Entry 
-            data={props.data}
-          />
-        </Col>
-        <Col>
-          <Entry 
-            data={props.data}
-          />
-        </Col>
-        <Col>
-          <Entry 
-            data={props.data}
-          />
-        </Col>
-        <Col>
-          {getEntry(1)}
+          {`entries: ${entryArr}`}
         </Col>
       </Row>
     </Container>
